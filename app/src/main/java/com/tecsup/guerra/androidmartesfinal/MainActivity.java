d55960d1b45b2b3d16077caa3f5bbc8ad477efb1
Jose Guerra <jose.guerra.g@tecsup.edu.pe>
2018-06-04 21:55:46 -0500

    firebase inicio

--- app/src/main/java/com/tecsup/guerra/androidmartesfinal/MainActivity.java
+++ app/src/main/java/com/tecsup/guerra/androidmartesfinal/MainActivity.java
@@ -3,11 +3,29 @@ package com.tecsup.guerra.androidmartesfinal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fullname", "Danilo Lopez");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle);
+
+        mFirebaseAnalytics.setUserProperty("username", "dlopez");
+
     }
 }
